db9813693b895982395e45f4ac1cf5e79d99c1b4
JuanSaldana <jasaldanah@gmail.com>
2022-06-05 15:36:56 -0500

    feat: email detail view added

--- project_3/mail/mail/static/mail/inbox.js
+++ project_3/mail/mail/static/mail/inbox.js
@@ -55,10 +55,40 @@ function compose_email() {
     .addEventListener("submit", send_email);
 }
 
+function read_email(email_id) {
+  console.log(email_id);
+  fetch(`/emails/${email_id}`)
+    .then((response) => response.json())
+    .then((email) => {
+      console.log(email);
+      document.querySelector("#email-view").style.display = "block";
+      document.querySelector("#emails-view").style.display = "none";
+
+      email_view = document.querySelector("#email-view");
+      email_view.innerHTML = `
+      <div class="full-email-header">
+      <span class="full-email-subject">${email.subject}</span>
+      <div class="full-email-subheader">
+      <span class="full-email-sender">${email.sender}</span>
+        <span class="full-email-date">${email.timestamp}</span>
+      </div>
+        <div class="full-email-body" type="box">${email.body}</div>
+      `;
+    });
+
+  fetch(`/emails/${email_id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      read: true,
+    }),
+  });
+}
+
 function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector("#emails-view").style.display = "block";
   document.querySelector("#compose-view").style.display = "none";
+  document.querySelector("#email-view").style.display = "none";
 
   // Query emails
   fetch(`/emails/${mailbox}`)
@@ -68,7 +98,7 @@ function load_mailbox(mailbox) {
       for (email of result) {
         emails_view.innerHTML += `
         <hr class="email-hr">
-        <div class="email" read=${email.read}>
+        <div class="email" read=${email.read} id=${email.id}>
         <div class="email-header">
           <span class="email-subject">${email.subject}</span>
           <div class="email-subheader">
@@ -79,6 +109,12 @@ function load_mailbox(mailbox) {
         <div class="email-body">${email.body}</div>
       </div>
         `;
+      }
+
+      // Add event listeners to each email
+      for (email of document.querySelectorAll(".email")) {
+        email_id = email.getAttribute("id");
+        email.addEventListener("click", read_email.bind(null, email_id));
       }
       console.log(result);
     });
