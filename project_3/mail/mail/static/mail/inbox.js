ee84497f426d096476df1cf4a3e8c995c7309acc
JuanSaldana <jasaldanah@gmail.com>
2022-06-07 19:02:03 -0500

    feat: read and reply email

--- project_3/mail/mail/static/mail/inbox.js
+++ project_3/mail/mail/static/mail/inbox.js
@@ -9,6 +9,14 @@ document.addEventListener("DOMContentLoaded", function () {
   document
     .querySelector("#archived")
     .addEventListener("click", () => load_mailbox("archive"));
+  document.querySelector("#back-button").addEventListener("click", () => {
+    email_view = document.querySelector("#full-email-view");
+    email_view.style.display = "none";
+    document.querySelector("#emails-view").style.zindex = "10";
+  });
+  document.querySelector("#reply-button").addEventListener("click", () => {
+    reply_email(email_id);
+  });
   document.querySelector("#compose").addEventListener("click", compose_email);
 
   // By default, load the inbox
@@ -42,6 +50,7 @@ function send_email(event) {
 function compose_email() {
   // Show compose view and hide other views
   document.querySelector("#emails-view").style.display = "none";
+  document.querySelector("#full-email-view").style.display = "none";
   document.querySelector("#compose-view").style.display = "block";
 
   // Clear out composition fields
@@ -55,27 +64,54 @@ function compose_email() {
     .addEventListener("submit", send_email);
 }
 
+function reply_email(email_id) {
+  fetch(`/emails/${email_id}`)
+    .then((response) => response.json())
+    .then((email) => {
+      compose_email();
+      document.querySelector("#compose-recipients").value = email.sender;
+      document.querySelector("#compose-subject").value = `Re: ${email.subject}`;
+      document.querySelector("#compose-body").value = `
+      On ${email.timestamp}, ${email.sender} wrote:
+      ${email.body}
+      `;
+    });
+}
+
 function read_email(email_id) {
   console.log(email_id);
   fetch(`/emails/${email_id}`)
     .then((response) => response.json())
     .then((email) => {
       console.log(email);
-      document.querySelector("#email-view").style.display = "block";
-      document.querySelector("#emails-view").style.display = "none";
+      document.querySelector("#full-email-view").style.display = "none";
+      document.querySelector("#full-email-view").style.display = "block";
+      document.querySelector("#emails-view").style.zindex = "-10";
+      back_button = document.querySelector("#back-button");
 
-      email_view = document.querySelector("#email-view");
+      email_view = document.querySelector("#full-email-view");
       email_view.innerHTML = `
-      <div class="full-email-header">
-      <span class="full-email-subject">${email.subject}</span>
-      <div class="full-email-subheader">
-      <span class="full-email-sender">${email.sender}</span>
-        <span class="full-email-date">${email.timestamp}</span>
-      </div>
+      <div class="full-email">
+        <div class="full-email-header">
+          <span class="full-email-subject">${email.subject}</span>
+          <div class="full-email-subheader">
+            <span class="full-email-sender">${email.sender}</span>
+            <span class="full-email-date">${email.timestamp}</span>
+          </div>
+        </div>
         <div class="full-email-body" type="box">${email.body}</div>
+        <div class="full-email-footer">
+          <button class="btn btn-primary" id="reply-button">Reply</button>
+        </div>
+      </div>
       `;
+      email_view.appendChild(back_button);
+      document.querySelector("#reply-button").addEventListener("click", () => {
+        reply_email(email_id);
+      });
     });
 
+  console.log("Marking as read");
   fetch(`/emails/${email_id}`, {
     method: "PUT",
     body: JSON.stringify({
@@ -88,7 +124,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector("#emails-view").style.display = "block";
   document.querySelector("#compose-view").style.display = "none";
-  document.querySelector("#email-view").style.display = "none";
+  document.querySelector("#full-email-view").style.display = "none";
 
   // Query emails
   fetch(`/emails/${mailbox}`)
